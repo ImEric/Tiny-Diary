a1d384ab8a9f05613c8a1030051ebb62e83a154e
Eric Hu <erichuju@gmail.com>
2015-09-26 15:27:46 -0700

    Initial Commit

---
+++ TinyCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinyCal
+//
+//  Created by ERIC on 9/26/15.
+//  Copyright © 2015 Eric Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
